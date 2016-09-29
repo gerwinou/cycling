44e7b4091d3ba75e0117e8cf811065e7072c0838
Gerwin Bastiaansen <gerwinou@gmail.com>
2016-09-29 13:58:21 +0200

    updates

--- python/fitbitFunctions.py
+++ python/fitbitFunctions.py
@@ -1,27 +1,3 @@
-'''
-The following lines are provided as a reference for how to do a request, and how to get the refresh_token
-
-curl -i -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0N1E4TEYiLCJhdWQiOiIyMjdTVEciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDc0NjY2MjE1LCJpYXQiOjE0NzQ2Mzc0MTV9.bi_BRBVNGi-LVirPq9XsW5_mBVyNSCERuPDcwthAQhs" https://api.fitbit.com/1/user/-/profile.json
-
-eyJhbGciOiJIUzI1NiJ9eyJzdWIiOiI0N1E4TEYiLCJhdWQiOiIyMjdTVEciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDc0NjY2MjE1LCJpYXQiOjE0NzQ2Mzc0MTV9bi/vv71EFU0aL++/vVYqz6vvv73vv71b77+977+9BVzvv71IIRHvv73vv73vv73vv73vv71AQhs=
-
-Refresh token
-curl -X POST -i -H "Authorization: Basic MjI3U1RHOjIxZGRmNWRiNTU5NTFiY2ViZTY4OWU1MGZjYWFjOThj" -H "Content-Type: application/x-www-form-urlencoded"
--d "grant_type=refresh_token" -d "refresh_token=f1a4a36f7ea84077807596e224fca71319c6627c50af772b108df36a18571ece" https://api.fitbit.com/oauth2/token
-
-{"access_token":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0N1E4TEYiLCJhdWQiOiIyMjdTVEciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiO
-iJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDc0Nj
-Y2MjE1LCJpYXQiOjE0NzQ2Mzc0MTV9.bi_BRBVNGi-LVirPq9XsW5_mBVyNSCERuPDcwthAQhs",
-"expires_in":28800,
-"refresh_token":"c472f65d0ff7104eb264e4a89f8f0d0d8ac44026c29f3657f7186461b53e705c","scope":"profile activity settings social location weight sleep heartrate nutrition","token_type":"Bearer","user_id":"47Q8LF"}
-
-{"access_token":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0N1E4TEYiLCJhdWQiOiIyMjdTVEciLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNDc0NjY3NDQyLCJpYXQiOjE0NzQ2Mzg2NDJ9.Go0_BJkAyukoKFu009p0qrLSfApJ9hupk3ns-ejph-I",
-"expires_in":28800,
-"refresh_token":"df61aec4473d87a94db7d144593780c7ba2d8f7f0882c72ed6ff7ccb2e78bbf4",
-"scope":"social activity nutrition settings location heartrate profile sleep weight","token_type":"Bearer","user_id":"47Q8LF"}
-
-'''
-
 import requests
 import configparser
 from datetime import datetime
@@ -30,6 +6,9 @@ import logging
 import logging.config
 import json
 import base64
+
+# TODO: - generalize requests
+# TODO: - handle refresh action; reset the AT en RT within
 
 logging.config.fileConfig('fitbitlogging.conf')
 logger = logging.getLogger('fitbitClient')
